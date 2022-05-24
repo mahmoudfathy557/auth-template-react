ff6b82abb0d8a3fae73a917d9a0d88a2593c63a8
mahmoudfathy557 <mahmoudfathy557@gmail.com>
2022-05-24 17:20:07 +0200

    updating backend baseAPI

--- src/context.js
+++ src/context.js
@@ -21,8 +21,8 @@ const IntranetProvider = ({ children }) => {
         }
   )
 
-  const baseAPI = 'http://localhost:8002'
-  // const baseAPI = 'https://dmgian.corp-dmg.com:8002'
+  // const baseAPI = 'http://localhost:8002'
+  const baseAPI = 'https://survey-platform-67zi2.ondigitalocean.app/api'
 
   const login = async (username, password) => {
     setUser({
@@ -326,6 +326,26 @@ const IntranetProvider = ({ children }) => {
       })
     }
   }
+
+  const DIGITALOCEAN_TOKEN =
+    'dop_v1_f887af1de77287258a6bfef9545228383872a99fb8b59962123377e8b33bb781'
+  const fetchData = async () => {
+    const result = await axios.get(
+      'https://api.digitalocean.com/v2/databases',
+      {
+        headers: {
+          Authorization: `Bearer ${DIGITALOCEAN_TOKEN}`,
+        },
+      }
+    )
+    return result.data
+  }
+
+  useEffect(() => {
+    fetchData().then((res) => {
+      console.log(res)
+    })
+  }, [])
 
   return (
     <IntranetContext.Provider
