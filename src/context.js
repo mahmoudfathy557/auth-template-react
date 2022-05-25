8feda0dc9ef52fa5623210a9097f58a00568a5c3
mahmoudfathy557 <mahmoudfathy557@gmail.com>
2022-05-25 14:54:23 +0200

    modifing baseAPI url

--- src/context.js
+++ src/context.js
@@ -21,7 +21,7 @@ const IntranetProvider = ({ children }) => {
         }
   )
 
-  // const baseAPI = 'http://localhost:8002'
+  // const baseAPI = 'http://localhost:4000'
   const baseAPI = 'https://survey-platform-67zi2.ondigitalocean.app/api'
 
   const login = async (username, password) => {
